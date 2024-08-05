d660c50e9ded2c567ab4d3c5f9b8fc07929e18cc
kumar485 <kumar485hyd@gmail.com>
2024-08-05 12:31:21 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,58 @@
+#object is a datatype ,
+# everthing in python is a object
+# way of storing data
+
+'susi'
+"shusi"
+"""shusi"""
+
+# A literal is pice of syntax that creates a datatype
+
+# A immutable sequesnce of text characters is a string
+
+# Any keyborad character you type comes under text
+# Incable of change , which in these case is object in place after creation
+" " # valid space in valid string
+"" # valid stirng empty sting
+'' # valid empty string
+
+""" this 
+is 
+my
+name
+"""
+#multiline string break text in multiple lines
+# they are using in documentation
+
+
+
+#intro in function
+
+# a sequence of steps
+# a prcedure
+# series of instruction
+# origanize code to be reused
+
+
+
+# bulit in
+#custom function
+
+
+#input and output
+
+#aruments , function can take inputs which is called arguments
+# return values
+
+print("hello")
+print("3 + 4 ") # 3 + 4
+print("3" + "4") #sting concatination
+
+#outputting muliple arguments in print statement
+
+
+print("a","b","c")  # community statndard  , and then space
+print("c", "d", "e", "f", 2+5, 4*4)
+print("c", "d", "e", "f", 2+5, 4*4,sep='*') #fallback is ''
+print("j","u ",end='**************')
+print("r","k")
